f9836d7bf53f9d6443b6e0af6f6c8303bf9ab804
Maxwell <max15borges@gmail.com>
2020-07-31 00:03:02 -0300

    correcoes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 -m server flask run -p $PORT -h 0.0.0.0
+web: python3 -m server flask run
--- server.py
+++ server.py
@@ -24,4 +24,4 @@ def home():
     return Response(r.iter_content(chunk_size=1024), mimetype='audio/mpeg')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port='8000', debug=True)
+    app.run(debug=True)
