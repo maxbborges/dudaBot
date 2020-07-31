2be47c5075d8e36ab0d0c81e76d2d80e950cf8e2
Maxwell <max15borges@gmail.com>
2020-07-30 23:43:09 -0300

    correcao

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, Response, request
 from funDownload import download
 from time import time
+import requests
 import json
 
 app = Flask(__name__, static_url_path='')
